e5c2fee1a59facc6dcff441a41af40c186060cf1
Douglas <douglasfigsouza@gmail.com>
2017-08-30 14:19:37 -0300

    Normalizando

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CursoJavaScript.MvcApplication" Language="C#" %>
---
+++ Scripts/Site.js
@@ -0,0 +1,11 @@
+﻿//teste
+function imc() {
+    var form = document.getElementById("formulario");
+    var kg = form.kilos.value;
+    var mt = form.metros.value;
+    var cm = +form.cm.value;
+    var altura = ((mt * 100) + cm) / 100;
+
+    var imc = kg / (altura * altura);
+    document.getElementById('resultado').textContent = imc;
+}
